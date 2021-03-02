8e618aff4737a57aac7cde40ac969fac3b016d80
unknown <unknown>
2021-03-02 14:18:37 +0000

    added one lick and update .ignore for replay

--- Action.c
+++ Action.c
@@ -5,6 +5,8 @@
 Action()
 {
 	truclient_step("1", "Navigate to 'http://www.advantageonlineshopping.com/'", "snapshot=Action_1.inf");
+	truclient_step("2", "Click on HEADPHONES", "snapshot=Action_2.inf");
+	truclient_step("3", "Click on HOME link", "snapshot=Action_3.inf");
 
 	return 0;
 }
