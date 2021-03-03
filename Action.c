8ffee83fe44cb9f667af114cb80f2afeb67a67b9
unknown <unknown>
2021-03-03 11:24:13 +0000

    added wait

--- Action.c
+++ Action.c
@@ -5,8 +5,7 @@
 Action()
 {
 	truclient_step("1", "Navigate to 'http://www.advantageonlineshopping.com/'", "snapshot=Action_1.inf");
-	truclient_step("2", "Click on HEADPHONES", "snapshot=Action_2.inf");
-	truclient_step("3", "Click on HOME link", "snapshot=Action_3.inf");
+	truclient_step("2", "Wait 3 seconds", "snapshot=Action_2.inf");
 
 	return 0;
 }
